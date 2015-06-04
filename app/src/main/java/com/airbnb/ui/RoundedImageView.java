a5be6a35601daa57c42eb65f248606e4179a3946
Gabriel Peal <gpeal@u.northwestern.edu>
2015-06-04 07:47:14 -0700

    RoundedImageView is now an ImageView
    Instead of RoundedImageView being a custom View that draws a bitmap,
    it is now actually an ImageView that can be used for libraries like
    Picasso, The image inside of RoundedImageView now also uses a
    CENTER_CROP-like scale type.

--- app/src/main/java/com/airbnb/ui/RoundedImageView.java
+++ app/src/main/java/com/airbnb/ui/RoundedImageView.java
@@ -1,87 +1,115 @@
 package com.airbnb.ui;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Shader;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.TypedValue;
+import android.widget.ImageView;
 
 import com.airbnb.R;
 
 /**
  * Custom view that displays a circular profile picture image with a stroke around it.
  */
-public class RoundedImageView extends View {
-    private static final int MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT;
+public class RoundedImageView extends ImageView {
 
-    private final int mDrawableSize;
-    private final Paint mBackgroundPaint;
-    private final Matrix mTranslationMatrix = new Matrix();
+    private final int mStrokeColor;
+    private final int mStrokeWidth;
+    private final Paint mStrokePaint = new Paint();
 
-    private Bitmap mBitmap;
-    private Paint mBitmapPaint;
+    private Bitmap mRoundedBitmap;
 
     public RoundedImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+    }
+
+    public RoundedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RoundedImageView);
-        int resId = ta.getResourceId(R.styleable.RoundedImageView_drawable, 0);
-        setImageBitmap(BitmapFactory.decodeResource(context.getResources(), resId));
-
-        mDrawableSize = ta.getDimensionPixelSize(R.styleable.RoundedImageView_drawableSize, MATCH_PARENT);
-        mBackgroundPaint = new Paint();
-        mBackgroundPaint.setStyle(Paint.Style.STROKE);
-        int strokeColor = ta.getColor(R.styleable.RoundedImageView_strokeColor, Color.WHITE);
-        mBackgroundPaint.setColor(strokeColor);
-        int strokeWidth = ta.getDimensionPixelSize(R.styleable.RoundedImageView_strokeWidth, 0);
-        mBackgroundPaint.setStrokeWidth(strokeWidth);
+        mStrokeColor = ta.getColor(R.styleable.RoundedImageView_strokeColor, Color.WHITE);
+        mStrokeWidth = ta.getDimensionPixelSize(R.styleable.RoundedImageView_strokeWidth, (int) dpToPx(2));
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setStrokeWidth(mStrokeWidth);
+        mStrokePaint.setColor(mStrokeColor);
         ta.recycle();
     }
 
-
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        if (mBitmap == null) {
+    protected void onDraw(@NonNull Canvas canvas) {
+        Drawable drawable = getDrawable();
+
+        if (drawable == null) {
             return;
         }
 
-        int drawableSize = (mDrawableSize == MATCH_PARENT) ?
-                (int) (Math.min(getWidth(), getHeight()) - 2 * mBackgroundPaint.getStrokeWidth())
-                : mDrawableSize;
-
-        if (mBitmapPaint == null) {
-            float aspectRatio = mBitmap.getHeight() / (float) mBitmap.getWidth();
-            Bitmap scaledBitmap = Bitmap.createScaledBitmap(
-                    mBitmap, mDrawableSize, (int) (drawableSize * aspectRatio), true);
-            mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            BitmapShader shader = new BitmapShader(
-                    scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-            mBitmapPaint.setShader(shader);
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
         }
 
-        int cx = getWidth() / 2;
-        int cy = getHeight() / 2;
-        int r = drawableSize / 2;
-        canvas.drawCircle(cx, cy, r, mBackgroundPaint);
-        int dx = getWidth() / 2 - drawableSize / 2;
-        int dy = getHeight() / 2 - drawableSize / 2;
-        mTranslationMatrix.setTranslate(dx, dy);
-        mBitmapPaint.getShader().setLocalMatrix(mTranslationMatrix);
-        canvas.drawCircle(cx, cy, r, mBitmapPaint);
+        if (mRoundedBitmap == null) {
+            Bitmap bitmap =  ((BitmapDrawable) drawable).getBitmap();
+            int scaledWidth;
+            int scaledHeight;
+            if (getWidth() > getHeight()) {
+                scaledWidth = getWidth() - 2 * mStrokeWidth;
+                scaledHeight = (int) (scaledWidth * (getHeight() / (float) getWidth()));
+            } else {
+                scaledHeight = getHeight() - 2 * mStrokeWidth;
+                scaledWidth = (int) (scaledHeight * (getWidth() / (float) getHeight()));
+            }
+
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, scaledWidth, scaledHeight, false);
+
+
+            mRoundedBitmap = getCircleBitmap(scaledBitmap);
+        }
+
+        canvas.drawBitmap(mRoundedBitmap, mStrokeWidth, mStrokeWidth, null);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, Math.min(getWidth() / 2, getHeight() / 2) - mStrokeWidth, mStrokePaint);
     }
 
-    private void setImageBitmap(Bitmap bitmap) {
-        mBitmap = bitmap;
-        mBitmapPaint = null;
-        invalidate();
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        mRoundedBitmap = null;
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.WHITE);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
+    }
+
+    private float dpToPx(float dp) {
+        Resources res = getContext().getResources();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, res.getDisplayMetrics());
     }
 }
